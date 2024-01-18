2281919b177e77383c2fb1d2b7060b74b543fca6
Zhao  Qianchi <zhaoqi56@ug132.eecg.toronto.edu>
2024-01-18 14:03:26 -0500

    added listner source code without modification

--- deliver.c
+++ deliver.c
@@ -4,8 +4,7 @@
 //essential functions to focus on are: socket, bind, sendto, and recvfrom.
 
 //The "Datagram Sockets" section under "Client-Server Background" is the most
-//relevant sample code for this lab since you're required to use
-//UDP. 
+//relevant sample code for this lab since you're required to use UDP. 
 int main(){
     printf("hello world\n");
     return 0;
