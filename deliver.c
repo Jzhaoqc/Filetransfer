9a40a9ace0261d057f45edfc548bf54999492ce6
Zhao  Qianchi <zhaoqi56@ug156.eecg.toronto.edu>
2024-01-17 23:02:01 -0500

    added comments on functions needed

--- deliver.c
+++ deliver.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+//The networking libraries can be daunting at first, but the 
+//essential functions to focus on are: socket, bind, sendto, and recvfrom.
+
+//The "Datagram Sockets" section under "Client-Server Background" is the most
+//relevant sample code for this lab since you're required to use
+//UDP. 
 int main(){
     printf("hello world\n");
     return 0;
