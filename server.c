fc6748e8b871cb60365ad18b096519dbbf937e8f
Zhao  Qianchi <zhaoqi56@ug156.eecg.toronto.edu>
2024-01-19 00:04:22 -0500

    Server done, before debug

--- server.c
+++ server.c
@@ -33,12 +33,15 @@ int main(void){
     socklen_t addr_len;
     char s[INET6_ADDRSTRLEN];
 
-    memset(&hints, 0, sizeof hints);    //init struct
+    //this is the structure that specifies the criteria for selecting socket address returned in the list pointed by serviceinfo
+    memset(&hints, 0, sizeof hints);    //init struct  
     hints.ai_family = AF_INET; // set to AF_INET to use IPv4
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_flags = AI_PASSIVE; // use my IP
 
-    if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {         //get information from port 55000
+    //getaddrinfo allocates and init a linked list of addrinfo struct, for each network matches node and service criteria set in hints
+    //might have more than 1 list node if network host is multihomed etc. all linked by ai_next field
+    if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
@@ -46,40 +49,42 @@ int main(void){
     // loop through all the results and bind to the first we can
     for(p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
-            perror("listener: socket");
+            perror("server: socket");
             continue;
         }
         if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
             close(sockfd);
-            perror("listener: bind");
+            perror("server: bind");
             continue;
         }
 
         break;
     }
 
-    if (p == NULL) {
-        fprintf(stderr, "listener: failed to bind socket\n");
+    if (p == NULL) {    //check if there is a usable address found from earlier
+        fprintf(stderr, "server: failed to bind socket\n");
         return 2;
     }
 
     freeaddrinfo(servinfo);
 
-    printf("listener: waiting to recvfrom...\n");
+    printf("server: waiting to recvfrom...\n");
 
+    //read from UDP message queue
     addr_len = sizeof their_addr;
+    //read the first msg in queue and populate their_addr with source address
     if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&their_addr, &addr_len)) == -1) {
         perror("recvfrom");
         exit(1);
     }
 
-    printf("listener: got packet from %s\n",
+    printf("server: got packet from %s\n",
         inet_ntop(their_addr.ss_family,
             get_in_addr((struct sockaddr *)&their_addr),
             s, sizeof s));
-    printf("listener: packet is %d bytes long\n", numbytes);
+    printf("server: packet is %d bytes long\n", numbytes);
     buf[numbytes] = '\0';
-    printf("listener: packet contains \"%s\"\n", buf);
+    printf("server: packet contains \"%s\"\n", buf);
 
     close(sockfd);
 
