d89a9b116be426d216ba8ea07ffcc6db28b970f9
Zhao  Qianchi <zhaoqi56@ug132.eecg.toronto.edu>
2024-02-02 10:59:34 -0500

    update part 2

---
+++ Part2/deliver.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+//#define SERVERPORT "55000" // the port users will be connecting to
+
+//The networking libraries can be daunting at first, but the 
+//essential functions to focus on are: socket, bind, sendto, and recvfrom.
+
+
+//The "Datagram Sockets" section under "Client-Server Background" is the most
+//relevant sample code for this lab since you're required to use
+//UDP. 
+
+//talker
+int main(int argc, char *argv[]){
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+
+    char* ip;
+    char *port, *hostname;
+    char filePath[100] = "./"; 
+    char fileName[100];
+    char cmd[100]={0};
+
+    if (argc != 3) {
+        fprintf(stderr,"Wrong number of input: deliver <server address> <server port number>\n");
+        exit(1);
+    }
+
+    //extracting hostname from command line input for DNS lookup
+    hostname = argv[1];
+    port = argv[2];
+    printf("client: input port %s\n", port);
+    printf("client: input hostname %s\n", hostname);
+
+    //init addrinfo struct, populate target spec for reference in getaddrinfo()
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // set to AF_INET to use IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+
+    //result will populate servinfo
+    if ((rv = getaddrinfo(hostname, port, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and make a socket
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("client: socket ERROR");
+            continue;
+        }
+
+        //ip address sanity check
+        struct sockaddr_in *ip_access = (struct sockaddr_in *) p->ai_addr;
+        ip = inet_ntoa(ip_access->sin_addr);
+        printf("client: IP from DNS lookup is %s\n", ip);
+    
+        break;
+    }
+    if (p == NULL) {
+        fprintf(stderr, "deliver: failed to create socket\n");
+        return 2;
+    }
+    freeaddrinfo(servinfo);
+    
+    //After this point, we have gotten the server's address and all relative info
+    //can use the structure instance p in sendto() and recvfrom() functions for data transmission
+
+
+    //get path to file, check existence
+    printf("client: File to transfer: ftp <file name>\n");
+    scanf("%s %s", cmd, fileName);
+    if(strcmp(cmd,"ftp") != 0){     //check if the first command from user is "ftp" print error if not
+        perror("Wrong command: not ftp\n");
+        exit (1);
+    }
+    strcat(filePath,fileName);
+    //printf("%s\n", filePath);
+    if(access(filePath, F_OK) != 0){
+        perror("File doesn't exist\n");
+        exit (1);
+    }
+
+    //sending message "ftp" to server to request for file transfer
+    char msg[4] = "ftp";
+    if((numbytes = sendto(sockfd, msg, sizeof(msg), 0, p->ai_addr, p->ai_addrlen)) == -1){
+        perror("failed to send\n");
+        exit (1);
+    }
+    //sanity check
+    printf("client: sent %d bytes to %s\n", numbytes, argv[1]);
+
+    //block until server responds with either "yes" or "no"
+    char resposeBuf[100];
+    if ((numbytes = recvfrom(sockfd, resposeBuf, sizeof(resposeBuf), 0, p->ai_addr, &(p->ai_addrlen))) == -1) { //NOTE: the p->ai_addr field should have an empty struct
+        perror("recvfrom");                                                                                     //which will then gets populated, reused p since they should be the same
+        exit(1);
+    }
+
+    if(strcmp(resposeBuf, "yes") == 0){
+        printf("client: A file transfer can start.\n");
+    }else{
+        perror("server refused transfer\n");
+        exit (1);
+    }
+
+    // sanity check, can still send
+    //sendto(sockfd, "can still send\n", sizeof("can still send\n"), 0, p->ai_addr, p->ai_addrlen);
+
+    close(sockfd);
+
+    return 0;
+
+}
---
+++ Part2/server.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define MAXBUFLEN 100
+
+void *get_in_addr(struct sockaddr *sa){
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    perror("not an IPv4 connection, got IPv6\n");
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+//listener
+int main(int argc, char *argv[]){
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+    struct sockaddr_storage client_addr;
+    char buf[MAXBUFLEN] = {0};
+    socklen_t addr_len;
+    char s[INET6_ADDRSTRLEN];
+
+    char* port, *response;
+
+    if(argc != 2){
+        fprintf(stderr,"Wrong number of input: server <UDP listen port>\n");
+        exit(1);
+    }
+    port = argv[1]; //populate port from command line argument
+    // printf("%s\n", port);
+    // return 0;
+
+    //init and populate the structure that specifies the criteria for selecting socket address 
+    //returned in the list pointed by serviceinfo
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // set to AF_INET to use IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    //Idea: find available address, bind to socket so that the client can reach this server
+    //getaddrinfo allocates and init a linked list of addrinfo struct, for each network matches node and service criteria set in hints
+    //might have more than 1 list node if network host is multihomed etc. all linked by ai_next field
+
+    if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {   //should return 0 unless error
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+            perror("server: socket");
+            continue;
+        }
+        //we do not want the server address to change, therefore binding to it
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+        break;
+    }
+    if (p == NULL) {    //check if there is a usable address found from earlier
+        fprintf(stderr, "server: failed to bind socket\n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("server: listening to port %s...\n", port);
+
+
+    //after this point, the address is binded and server is listening and waiting to recieve
+    //after recieving the structure client_addr would get populated and can be used to respond
+
+
+    //read from UDP message queue
+    addr_len = sizeof client_addr;
+    //read the first msg in queue and populate client_addr with source address
+    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&client_addr, &addr_len)) == -1) {
+        perror("recvfrom");
+        exit(1);
+    }
+
+    // //debug
+    // printf("%s\n", buf);
+    // int result = strcmp(buf, "ftp");
+    // printf("%d\n", result);
+    // return 0;
+
+    printf("server: got packet from %s\n",
+        inet_ntop(client_addr.ss_family,
+            get_in_addr((struct sockaddr *)&client_addr),
+            s, sizeof s));
+    printf("server: packet is %d bytes long\n", numbytes);
+    buf[numbytes] = '\0';
+    printf("server: packet contains message \"%s\"\n", buf);
+
+
+    //check if the content is "ftp", reply with result
+    if(strcmp(buf, "ftp") == 0){ 
+        response = "yes";
+    }else{ 
+        response = "no";
+    }
+
+    ssize_t responseByte;
+    if( (responseByte = sendto(sockfd, response, sizeof(response), 0, (struct sockaddr *)&client_addr, addr_len)) == -1 ){
+        perror("response sendto");
+        exit(1);
+    }
+
+    // sanity check, can still recieve
+    // recvfrom(sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&client_addr, &addr_len);
+    // printf("%s", buf);
+
+    close(sockfd);
+
+    return 0;
+}
