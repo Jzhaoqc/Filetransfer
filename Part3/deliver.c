2c8ecb950c71ea6b7afc2bca15aaadb2dc6b58a9
Jzhaoqc <zhaoqianchi20021008@gmail.com>
2024-02-24 22:32:11 -0500

    added lab 3

---
+++ Part3/deliver.c
@@ -0,0 +1,254 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <time.h>
+
+//#define SERVERPORT "55000" // the port users will be connecting to
+#define MAX_ARRAY_SIZE 5000
+
+// define packet, for a file that is larger than 1000 bytes, 
+// needs to be fragmented and use multiple packets
+struct packet {
+    unsigned int total_frag;
+    unsigned int frag_no;
+    unsigned int size;
+    char* filename;
+    char filedata[1000];
+};
+
+
+//global array, used to store packets of files to send to server
+struct packet packetsBuffer[MAX_ARRAY_SIZE]={0};
+
+
+size_t findSize(FILE *fptr){
+
+    if(fptr == NULL){
+        perror("Can not open file");
+        exit(1);
+    }
+    if(fseek(fptr,0,SEEK_END)<0){
+        fclose(fptr);
+        perror("fseek failure");
+        exit(1);
+    }
+    size_t size = ftell(fptr);
+    printf("client: file size is %d\n", size);
+    return size;
+}
+
+int findPacketNumber(size_t size){
+    if((size%1000)!=0){
+        return (size/1000)+1;
+    }else{
+        return size/1000;
+    }
+}
+
+void populatePacketsBuffer(FILE* fptr, char* fileName, int totalFrag){
+    size_t bytesRead = 0;
+    int bufferIndex = 0;
+
+    while ((bytesRead = fread(packetsBuffer[bufferIndex].filedata, 1, 1000, fptr)) > 0){
+        packetsBuffer[bufferIndex].filename = malloc(strlen(fileName) + 1); // Allocate memory
+        if (packetsBuffer[bufferIndex].filename == NULL) {
+            perror("Memory allocation failed\n");
+            exit(1);
+        }
+        strcpy(packetsBuffer[bufferIndex].filename, fileName);
+        
+        packetsBuffer[bufferIndex].total_frag = totalFrag;
+        packetsBuffer[bufferIndex].frag_no = bufferIndex + 1;
+        packetsBuffer[bufferIndex].size = bytesRead;
+
+        bufferIndex++;
+        
+        if (bufferIndex >= MAX_ARRAY_SIZE){
+            perror("Packet Buffer too small\n");
+            exit (1);
+        }
+    }
+}
+
+
+//converting the packet struct into actual packet with fields seperated by ":"
+void constructPacket(int index, char sendBuffer[]){
+    int headerlen = snprintf(sendBuffer,2000,"%u:%u:%u:%s:", packetsBuffer[index].total_frag, 
+                                    packetsBuffer[index].frag_no, packetsBuffer[index].size, 
+                                    packetsBuffer[index].filename);
+
+    if(headerlen == 0){
+        perror("unable to snprint\n");
+        exit (1);
+    }
+
+    memcpy(sendBuffer+headerlen, packetsBuffer[index].filedata, packetsBuffer[index].size);
+    return;
+}
+
+//talker
+int main(int argc, char *argv[]){
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+
+    char* ip;
+    char *port, *hostname;
+    char filePath[100] = "./send/"; 
+    char fileName[100];
+
+    char cmd[100]={0};
+
+    //check number of input
+    if (argc != 3) {
+        fprintf(stderr,"Wrong number of input: deliver <server address> <server port number>\n");
+        exit(1);
+    }
+
+    //extracting hostname from command line input for DNS lookup
+    hostname = argv[1];
+    port = argv[2];
+
+    printf("client: input port %s\n", port);
+    printf("client: input hostname %s\n", hostname);
+
+    //init addrinfo struct, populate target spec for reference in getaddrinfo()
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // set to AF_INET to use IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+
+    //result will populate servinfo
+    if ((rv = getaddrinfo(hostname, port, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and make a socket
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("client: socket ERROR");
+            continue;
+        }
+
+        //ip address sanity check
+        struct sockaddr_in *ip_access = (struct sockaddr_in *) p->ai_addr;
+        ip = inet_ntoa(ip_access->sin_addr);
+        printf("client: IP from DNS lookup is %s\n", ip);
+    
+        break;
+    }
+    if (p == NULL) {
+        fprintf(stderr, "deliver: failed to create socket\n");
+        return 2;
+    }
+    freeaddrinfo(servinfo);
+    
+    //After this point, we have gotten the server's address and all relative info
+    //can use the structure instance p in sendto() and recvfrom() functions for data transmission
+
+
+    //get path to file, check existence
+    printf("client: File to transfer: ftp <file name>\n");
+    
+    //debug comment out
+    scanf("%s %s", cmd, fileName);
+    if(strcmp(cmd,"ftp") != 0){     //check if the first command from user is "ftp" print error if not
+        perror("Wrong command: not ftp\n");
+        exit (1);
+    }
+
+    strcat(filePath,fileName);
+    printf("client: trying to access: %s\n", filePath);
+    if(access(filePath, F_OK) != 0){
+        perror("File doesn't exist\n");
+        exit (1);
+    }
+
+    //sending message "ftp" to server to request for file transfer
+    char msg[4] = "ftp";
+    if((numbytes = sendto(sockfd, msg, sizeof(msg), 0, p->ai_addr, p->ai_addrlen)) == -1){
+        perror("failed to send ftp\n");
+        exit (1);
+    }
+    //sanity check
+    printf("client: sent %d bytes to %s\n", numbytes, argv[1]);
+
+    //block until server responds with either "yes" or "no"
+    char resposeBuf[100];
+    if ((numbytes = recvfrom(sockfd, resposeBuf, sizeof(resposeBuf), 0, p->ai_addr, &(p->ai_addrlen))) == -1) { //NOTE: the p->ai_addr field should have an empty struct
+        perror("recvfrom");                                                                                     //which will then gets populated, reused p since they should be the same
+        exit(1);
+    }
+
+    if(strcmp(resposeBuf, "yes") == 0){
+        printf("client: Connected. File transfer starts.\n");
+    }else{
+        perror("server refused transfer\n");
+        exit (1);
+    }
+
+    // sanity check, can still send
+    //sendto(sockfd, "can still send\n", sizeof("can still send\n"), 0, p->ai_addr, p->ai_addrlen);
+
+    //after established connection with the server, we now get the file, 
+    //transform it to packet(s) if data is larger than 1000bytes, and send to server
+
+    //open file
+    FILE *fptr = fopen(filePath, "rb");   //rb stand for read binary
+    size_t fileSize = findSize(fptr);
+    int totalFrag = findPacketNumber(fileSize);
+    //parse content, populate packet buffer and close at the end
+    fseek(fptr, 0, SEEK_SET); //reset stream position to beginning and begin fragmentation
+    populatePacketsBuffer(fptr, fileName, totalFrag);
+    fclose(fptr);
+
+    //after this point we have the buffer with all the packet structs transformed into sendable format
+
+    //we start the timer for measuring the round trip time
+    struct timespec start, end;
+    long long time_difference;
+    clock_gettime(CLOCK_MONOTONIC, &start); // define the timespec, and get the start time stamp
+
+    //then start sending
+    char sendBuffer[2000]={0};
+    for(int i=0; i<totalFrag; i++){
+        constructPacket(i,sendBuffer);
+
+
+        if((numbytes = sendto(sockfd, sendBuffer, sizeof(sendBuffer), 0, p->ai_addr, p->ai_addrlen)) == -1){
+            perror("failed to send packet send buffer\n");
+            exit (1);
+        }
+    }
+    printf("client: Sent %s to %s, %d packets total.\n", fileName, hostname, totalFrag);
+
+
+    if ((numbytes = recvfrom(sockfd, resposeBuf, sizeof(resposeBuf), 0, p->ai_addr, &(p->ai_addrlen))) == -1) { //NOTE: the p->ai_addr field should have an empty struct
+        perror("recvfrom");                                                                                     //which will then gets populated, reused p since they should be the same
+        exit(1);
+    }
+
+    if(strcmp(resposeBuf, "ACK") == 0){
+        printf("client: Server acknowledged.\n");
+    }else{
+        perror("client: Server did not acknowledge.\n");
+        exit (1);
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    time_difference = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec); //sec returns seconds, nsec for nano_seconds, more pricise
+    printf("client: RTT is %f milliseconds\n", time_difference / 1000000.0);// convert nanoseoncd to milisecond
+
+    close(sockfd);
+
+    return 0;
+
+}
---
+++ Part3/server.c
@@ -0,0 +1,229 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/stat.h>
+
+#define MAXBUFLEN 100
+#define MAX_ARRAY_SIZE 5000
+
+// define packet, for a file that is larger than 1000 bytes, 
+// needs to be fragmented and use multiple packets
+struct packet {
+    unsigned int total_frag;
+    unsigned int frag_no;
+    unsigned int size;
+    char* filename;
+    char filedata[1000];
+};
+
+struct packet receivedPackets[MAX_ARRAY_SIZE]={0};
+
+void writeFile(){
+    // Define the directory path
+    const char* directory = "./received_files";
+
+    // Create the directory if it doesn't exist
+    if (mkdir(directory, 0777) == -1 && errno != EEXIST) {
+        perror("mkdir");
+        exit(EXIT_FAILURE);
+    }
+ 
+    // Construct the file path
+    char filepath[1024];
+    snprintf(filepath, sizeof(filepath), "%s/%s", directory, receivedPackets[0].filename);
+
+    // Open the file in binary append mode
+    FILE *file = fopen(filepath, "w");
+    if(file == NULL){
+        perror("Cannot create new file.\n");
+        exit(1);
+    }
+    
+    //move data to file created
+    for(int i=0; i<receivedPackets[i].total_frag; i++){
+        fwrite(receivedPackets[i].filedata, 1, receivedPackets[i].size, file);
+    }
+    fclose(file);
+    
+    printf("server: finished writing to file.\n");
+}
+
+//function to recieve and read from UDP message queue and populate receivedPackets[]
+void receivePackets(int sockfd, struct sockaddr_storage client_addr, socklen_t addr_len) {
+    char recvBuffer[2000] = {0};
+    int recvBufferIndex = -1;
+    
+    // Keep receiving packets and build the file until it is the last one
+    do {
+        recvBufferIndex++;
+        size_t receivedBytes = 0;
+        if ((receivedBytes = recvfrom(sockfd, recvBuffer, 2000 , 0, (struct sockaddr *)&client_addr, &addr_len)) == -1) {
+            perror("recvfrom for packets");
+            exit(1);
+        }
+
+        int itemMatched = 0;
+        char filenameBuffer[1024];
+
+        //after we take the message from UDP buffer to our own, we scan from the receive buffer and see if the format is correct
+        // populate the first few fields within the struct in the global receivePackets[] buffer
+        if((itemMatched = sscanf(recvBuffer, "%u:%u:%u:%[^:]:", &(receivedPackets[recvBufferIndex].total_frag), &(receivedPackets[recvBufferIndex].frag_no),
+                                                          &(receivedPackets[recvBufferIndex].size), filenameBuffer)) < 0) {
+            perror("unable to scan from string received.\n");
+            exit(1);
+        }
+
+        //copy the file name field and copy the data portion
+        int offset = snprintf(NULL, 0, "%u:%u:%u:%s:", receivedPackets[recvBufferIndex].total_frag, receivedPackets[recvBufferIndex].frag_no, 
+                                                  receivedPackets[recvBufferIndex].size, filenameBuffer);
+        receivedPackets[recvBufferIndex].filename = strdup(filenameBuffer);
+        memcpy(receivedPackets[recvBufferIndex].filedata, recvBuffer+offset, receivedPackets[recvBufferIndex].size);
+
+        //debug
+        printf("server: receiving packet number %d out of %d.\n",receivedPackets[recvBufferIndex].frag_no, receivedPackets[recvBufferIndex].total_frag);
+
+    } while(receivedPackets[recvBufferIndex].total_frag != receivedPackets[recvBufferIndex].frag_no);
+
+}
+
+void *get_in_addr(struct sockaddr *sa){
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    perror("not an IPv4 connection, got IPv6\n");
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+//listener
+int main(int argc, char *argv[]){
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+    struct sockaddr_storage client_addr;
+    char buf[MAXBUFLEN] = {0};
+    socklen_t addr_len;
+    char s[INET6_ADDRSTRLEN];
+
+    char* port, *response;
+
+    if(argc != 2){
+        fprintf(stderr,"Wrong number of input: server <UDP listen port>\n");
+        exit(1);
+    }
+    port = argv[1]; //populate port from command line argument
+
+    // printf("%s\n", port);
+    // return 0;
+
+    //init and populate the structure that specifies the criteria for selecting socket address 
+    //returned in the list pointed by serviceinfo
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // set to AF_INET to use IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    //Idea: find available address, bind to socket so that the client can reach this server
+    //getaddrinfo allocates and init a linked list of addrinfo struct, for each network matches node and service criteria set in hints
+    //might have more than 1 list node if network host is multihomed etc. all linked by ai_next field
+
+    if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {   //should return 0 unless error
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+            perror("server: socket");
+            continue;
+        }
+        //we do not want the server address to change, therefore binding to it
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+        break;
+    }
+    if (p == NULL) {    //check if there is a usable address found from earlier
+        fprintf(stderr, "server: failed to bind socket\n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("server: listening to port %s...\n", port);
+
+
+    //after this point, the address is binded and server is listening and waiting to recieve
+    //after recieving the structure client_addr would get populated and can be used to respond
+
+
+    //read from UDP message queue
+    addr_len = sizeof client_addr;
+    //read the first msg in queue and populate client_addr with source address
+    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&client_addr, &addr_len)) == -1) {
+        perror("recvfrom");
+        exit(1);
+    }
+
+    // //debug
+    // printf("%s\n", buf);
+    // int result = strcmp(buf, "ftp");
+    // printf("%d\n", result);
+    // return 0;
+
+    printf("server: got packet from %s\n",
+        inet_ntop(client_addr.ss_family,
+            get_in_addr((struct sockaddr *)&client_addr),
+            s, sizeof s));
+    printf("server: packet is %d bytes long\n", numbytes);
+    buf[numbytes] = '\0';
+    printf("server: packet contains message \"%s\"\n", buf);
+
+
+    //check if the content is "ftp", reply with result
+    if(strcmp(buf, "ftp") == 0){ 
+        response = "yes";
+    }else{ 
+        response = "no";
+    }
+
+    ssize_t responseByte;
+    if( (responseByte = sendto(sockfd, response, sizeof(response), 0, (struct sockaddr *)&client_addr, addr_len)) == -1 ){
+        perror("response sendto");
+        exit(1);
+    }
+
+    // sanity check, can still recieve
+    // recvfrom(sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&client_addr, &addr_len);
+    // printf("%s", buf);
+
+    //after server allows to be connected, we wait again to recieve file from client
+
+    while(1){
+
+        receivePackets(sockfd, client_addr, addr_len);
+        writeFile();
+        //send ack 
+        char ack_buffer[]="ACK";
+        if( (responseByte = sendto(sockfd, ack_buffer, sizeof(ack_buffer), 0, (struct sockaddr *)&client_addr, addr_len)) == -1 ){
+            perror("response sendto");
+            exit(1);
+        }
+
+    }
+
+    close(sockfd);
+
+    return 0;
+}
